c4feb0a0e4fdd920ca54699db50bba000ba34d97
Andrew Bloyce <bloyce1990@gmail.com>
2022-11-20 15:26:11 +1000

    Testing more real content

--- scripts/create-blog-posts/index.js
+++ scripts/create-blog-posts/index.js
@@ -1,43 +1,61 @@
 const fs = require('fs')
 const path = require('path')
 const fm = require('front-matter')
+const MarkdownIt = require('markdown-it')
+const md = new MarkdownIt()
 
 const blogsMdPath = path.join(__dirname, "../../", 'blogs');
 const blogsLivePath = path.join(__dirname, "../../", "src/", "blog")
 
 // Empty the live blogs folder
-fs.readdir(blogsLivePath, (err, files) => {
-	if (err) {
-		console.log(err)
-	}
-	console.log(files)
-	files.forEach(file => {
-		console.log(file)
-		fs.rmSync(path.join(blogsLivePath, file), { recursive: true }, err => {
-			if (err) {
-				console.log(err)
-			}
+
+const deleteExistingBlogs = async () => {
+	fs.readdir(blogsLivePath, (err, files) => {
+		if (err) {
+			console.log(err)
+		}
+		console.log(files)
+		files.forEach(file => {
+			console.log(file)
+			fs.rmSync(path.join(blogsLivePath, file), { recursive: true }, err => {
+				if (err) {
+					console.log(err)
+				}
+			})
 		})
 	})
-})
+	return Promise.resolve()
+}
 
-fs.readdir(blogsMdPath, (err, files) => {
-	if (err) {
-		console.log(err)
-	}
-	files.forEach(file => {
-		fs.readFile(path.join(blogsMdPath, file), "utf-8", (err, data) => {
-			if (err) {
-				console.log(err)
-			}
-			const frontMatter = fm(data);
-			if (!fs.existsSync(`src/blog/${frontMatter.attributes.path}`)) {
-				fs.mkdirSync(`src/blog/${frontMatter.attributes.path}`)
-				fs.writeFile(`src/blog/${frontMatter.attributes.path}/index.html`, JSON.stringify(frontMatter), (err) => {
+const createBlogPages = async () => {
+	fs.readdir(blogsMdPath, (err, files) => {
+		console.log("blogs path", files)
+		if (err) {
+			console.log(err)
+		}
+		files.forEach(file => {
+			fs.readFile(path.join(blogsMdPath, file), "utf-8", (err, data) => {
+				if (err) {
+					console.log(err)
+				}
+				console.log("data", data)
+				const frontMatter = fm(data);
+				const contentFromMdToHTML = md.render(frontMatter.body);
+
+				fs.mkdirSync(`../../src/blog/${frontMatter.attributes.path}`, { recursive: true })
+				fs.writeFile(`../../src/blog/${frontMatter.attributes.path}/index.html`, contentFromMdToHTML, (err) => {
 					if (err) throw err;
 					console.log("Saved")
 				})
-			}
+			})
 		})
 	})
-})
+	return Promise.resolve()
+}
+
+const doTheMagic = async () => {
+	await deleteExistingBlogs()
+	await createBlogPages()
+}
+
+doTheMagic();
